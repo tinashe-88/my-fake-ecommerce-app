c60a679c53416455d9d2d91e8e411da590df8a27
Tinashe <tinashejchiweshe@gmail.com>
2020-07-23 17:00:10 +0200

    storing our user data using firebase in the main App.js component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 // Web app's Firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBA-ww_eCQPsWMWM1MivimyiVrfe0anoQw",
   authDomain: "my-shop-app-b8b9b.firebaseapp.com",
   databaseURL: "https://my-shop-app-b8b9b.firebaseio.com",
@@ -15,6 +15,31 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
+
+// Take userAuth obj and store inside firebase database
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('Error creating user', error.message)
+    }
+  }
+  return userRef
+}
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
