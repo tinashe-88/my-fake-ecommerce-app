d05aad1dc9fb6bcdaf4a02fe45b72827ffe47893
Tinashe <tinashejchiweshe@gmail.com>
2020-07-31 23:30:19 +0200

    collections-overview component created

--- src/pages/shop/shoppage.jsx
+++ src/pages/shop/shoppage.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
+import { Route } from 'react-router-dom'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import CollectionsOverview from '../../components/collections-overview/collections-overview'
 
-import { selectCollections } from '../../redux/shop/shop.selector'
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <>
-    {
-      collections.map(({ id, ...otherProps}) => (
-        <CollectionPreview 
-          key={id}
-          {...otherProps}
-        />
-      ))
-    }
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
   </>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage)
+export default ShopPage
