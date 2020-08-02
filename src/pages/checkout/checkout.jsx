abf34e59254eb70e4aaba96ba92776a00317cb8e
Tinashe <tinashejchiweshe@gmail.com>
2020-08-02 19:40:09 +0200

    favicon and logo updated

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -45,9 +45,9 @@ const CheckoutPage = ({ cartItems, total}) => (
         </TotalContainer>
 
         <TestWarningText>
-            *Please use the following test credit card details for payments*
+            *Please use the following test credit card details for testing payments*
             <br/>
-            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+            4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
         </TestWarningText>
         <StripeCheckoutButton price={total} />
     </CheckoutPageContainer>
