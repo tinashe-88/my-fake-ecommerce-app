ece9169416f6e04cd11a8cc2c44ff713bb1fcd80
Tinashe <tinashejchiweshe@gmail.com>
2020-08-02 13:49:30 +0200

    refactored shop data into an object and selectCollection shop selector implemeting data normalization

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,12 +1,5 @@
 import { createSelector } from 'reselect'
-
-const COLLECTION_ID = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
+import memoize from 'lodash.memoize'
 
 const selectShop = state => state.shop
 
@@ -15,9 +8,10 @@ export const selectCollections = createSelector(
   shop => shop.collections
 )
 
-export const selectCollection = collectionUrlParam => 
+export const selectCollection = memoize(collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections => collections.find(collection => 
-      collection.id === COLLECTION_ID[collectionUrlParam])
-  )
+    collections => 
+      collections ? collections[collectionUrlParam]
+      : null
+  ))
