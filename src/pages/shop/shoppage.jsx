1951a8cb2fe7c3bb1e9ee8514578779d9fa8950e
Tinashe <tinashejchiweshe@gmail.com>
2020-08-07 13:40:59 +0200

    installed redux-saga library and implemented sagas into the shop page

--- src/pages/shop/shoppage.jsx
+++ src/pages/shop/shoppage.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
 import CollectionPageContainer from '../../pages/collection/collection.container'
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview'
 
 class ShopPage extends React.Component {
   componentDidMount(){
-    const { fetchCollectionsStartAsync } = this.props
+    const { fetchCollectionsStart } = this.props
 
-    fetchCollectionsStartAsync()
+    fetchCollectionsStart()
   }
 
   render(){
@@ -34,7 +34,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage)
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,16 +4,22 @@ import {
 } from 'redux'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './root.reducer'
 
-const middleware = [thunk]
+import { fetchCollectionsStart } from './shop/shop.sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const middleware = [sagaMiddleware]
 
 if (process.env.NODE_ENV === 'development'){
   middleware.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware))
+
+sagaMiddleware.run(fetchCollectionsStart)
 
 export const persistor = persistStore(store)
