edf3799697afdeac2b4c4f6d1456ba1aff727c64
Tinashe <tinashejchiweshe@gmail.com>
2020-08-12 17:39:45 +0200

    express server added to handle backend stripe payment

--- src/components/footer/footer.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const FooterContainer = styled.footer`
-  padding-top: 5vh;
-  min-height: 100px;
-  text-align: center;
-  font-size: .7rem;
-  color: #777;
-`
