f74aa362169ed8100ed44ecb7db478ba7c8b1b98
Tinashe <tinashejchiweshe@gmail.com>
2020-08-17 14:14:25 +0200

    shop.reducer tests added

---
+++ client/src/redux/shop/shop.reducer.test.js
@@ -0,0 +1,58 @@
+import ShopActionTypes from './shop.types'
+import shopReducer from './shop.reducer'
+
+const initialState = {
+  collections: null,
+  isFetching: false,
+  errorMessage: undefined
+}
+
+describe('shopReducer', () => {
+  it('Should return initial state', () => {
+    expect(shopReducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('Should set isFetching to true if fetchingCollectionsStart action', () => {
+    expect(
+      shopReducer(initialState, {
+        type: ShopActionTypes.FETCH_COLLECTIONS_START
+      }).isFetching
+    ).toBe(true)
+  })
+
+  it('Should set isFetching to false and collections to payload if fetchingCollectionsSuccess', () => {
+    const mockItems = [
+      { id: 1 },
+      { id: 2 }
+    ]
+
+    expect(
+      shopReducer(
+        initialState, {
+          type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+          payload: mockItems
+        }
+      )
+    ).toEqual({
+      ...initialState,
+      isFetching: false,
+      collections: mockItems
+    })
+  })
+
+  it('Should set isFetching to false and errorMessage to payload if fetchingCollectionsFailure', () => {
+    expect(
+      shopReducer(
+        initialState, {
+          type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+          payload: 'error'
+        }
+      )
+    ).toEqual({
+      ...initialState,
+      isFetching: false,
+      errorMessage: 'error'
+    })
+  });
+  
+})
